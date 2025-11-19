18be0d3aafb0654c48264bf0eb3d1e57fe014d28
Zaeem Athar <zaeemathar94@gmail.com>
2025-11-19 17:30:58 +0100

    Adding helper functions.

---
+++ helper_functions/duck_show_models.py
@@ -0,0 +1,20 @@
+import duckdb
+import pandas as pd 
+
+TABLES = [
+    "dim_airplane",
+    "dim_airplane_model"
+    "dim_city",
+    "dim_customer_group",
+    "dim_customer",
+    "fact_order",
+    "fact_trip"
+]
+
+con = duckdb.connect("duckdb/database.duckdb")
+
+for table in TABLES:
+    res = pd.read_sql(f"SELECT * FROM {table}", con=con)
+    print(res.head(50))
+
+con.close()
